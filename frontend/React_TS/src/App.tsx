a1f790115a7adc7fb9b963afbed79ac21aae3142
speed <speedjudy928@gmail.com>
2023-02-19 20:19:08 +0900

    new

--- frontend/React_TS/src/App.tsx
+++ frontend/React_TS/src/App.tsx
@@ -1,43 +1,156 @@
-import * as React from 'react'; 
-import { BrowserRouter as Router, Route, Link,Switch } from "react-router-dom";
- 
-import './App.css'; 
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import 'toastr/build/toastr.min.css'; 
-import Index from './components/index.component';
-import Edit from './components/edit.component';
-import Create from './components/create.component';
-import Home from './components/home.component';
+import { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-const App: React.FC = () => {
-  return (
-      <Router>
-          <div className="container">
-              <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                      <ul className="navbar-nav mr-auto">
-                          <li className="nav-item">
-                              <Link to={'/'} className="nav-link">Home</Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link to={'/create'} className="nav-link">Create</Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link to={'/index'} className="nav-link">Index</Link>
-                          </li>
-                      </ul>
-                  </div>
-              </nav> <br/>
-         
-              <Switch>
-                  <Route exact path='/create' component={ Create } />
-                  <Route path='/edit/:id' component={ Edit } />
-                  <Route path='/index' component={ Index } />
-                  <Route path='/' component={ Home } />
-              </Switch>
+import AuthService from "./services/auth.service";
+import IUser from './types/user.type';
+
+import Login from "./components/login.component";
+import Register from "./components/register.component";
+import Home from "./components/home.component";
+import Profile from "./components/profile.component";
+import BoardUser from "./components/board-user.component";
+import BoardModerator from "./components/board-moderator.component";
+import BoardAdmin from "./components/board-admin.component";
+
+import EventBus from "./common/EventBus";
+
+type Props = {};
+
+type State = {
+  showModeratorBoard: boolean,
+  showAdminBoard: boolean,
+  currentUser: IUser | undefined
+}
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+        showModeratorBoard: user.role,
+        showAdminBoard: user.role,
+      });
+    }
+
+    EventBus.on("logout", this.logOut);
+  }
+
+  componentWillUnmount() {
+    EventBus.remove("logout", this.logOut);
+  }
+
+  logOut() {
+    AuthService.logout();
+    this.setState({
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    });
+  }
+
+  render() {
+    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/"} className="navbar-brand">
+            REACT
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </li>
+
+            {/* {showModeratorBoard && (
+              <li className="nav-item">
+                <Link to={"/mod"} className="nav-link">
+                  Moderator Board
+                </Link>
+              </li>
+            )}
+
+            {showAdminBoard && (
+              <li className="nav-item">
+                <Link to={"/admin"} className="nav-link">
+                  Admin Board
+                </Link>
+              </li>
+            )} */}
+
+            {currentUser && (
+              <li className="nav-item">
+                <Link to={"/rental"} className="nav-link">
+                  Rental
+                </Link>
+              </li>
+            )}
           </div>
-      </Router>
-  );
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/rental" element={<BoardUser />} />
+            <Route path="/mod" element={<BoardModerator />} />
+            <Route path="/admin" element={<BoardAdmin />} />
+          </Routes>
+        </div>
+
+        { /*<AuthVerify logOut={this.logOut}/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
